596207aa28c8150896657355559695ea92496476
KansouJellyfish <113874605+KansouJellyfish@users.noreply.github.com>
2025-03-19 00:18:06 +0800

    Update tiffconvert.m

--- tiffconvert.m
+++ tiffconvert.m
@@ -1,10 +1,10 @@
-clc;clear;close all;
-% 1. 调用 dcraw 读取 .ARW 并转换为 16-bit TIFF
-filepath='C:\example\'; % RAW格式文件路径
-filename='DSC0%04d.ARW';
-outputfile='DSC0%04d.tiff'
-for i=1111:2222
-    raw_filename = fullfile(filepath,sprintf(filename,i))
+clc;clear;
+% 调用 dcraw 读取 .ARW 并转换为 16-bit TIFF
+filepath='.\RAW\'; % RAW文件路径
+filename='DSC0%04d.ARW'; % 文件名，此处为索尼相机命名规范，此处使用%04d以方便批量处理
+outputfile='DSC0%04d.tiff';
+for i=0001:0004
+    raw_filename=fullfile(filepath,sprintf(filename,i));
     % 使用 dcraw 转换 RAW 为 16 位 TIFF
-    system(['dcraw -4 -v -w -T "', raw_filename, '"']); % -w表示使用相机拍摄时白平衡，-T表示以16位tiff输出
+    system(['dcraw -4 -v -w -T "', raw_filename, '"']); % -4代表输出线性文件，-v表示显示信息，-w代表使用相机白平衡，-T代表输出16 bit tiff
 end
